bafbb4537a342e6ab6f185839926ff62910246e6
kylegav <60828879+kylegav@users.noreply.github.com>
2022-03-07 17:11:02 -0500

    Database integration for NSObjects

---
+++ ChatApp/Chats+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Chats+CoreDataClass.swift
+//  ChatApp
+//
+//  Created by Kyle Gavin on 3/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Chats)
+public class Chats: NSManagedObject {
+
+}
---
+++ ChatApp/Chats+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Chats+CoreDataProperties.swift
+//  ChatApp
+//
+//  Created by Kyle Gavin on 3/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Chats {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Chats> {
+        return NSFetchRequest<Chats>(entityName: "Chats")
+    }
+
+    @NSManaged public var msg: String?
+    @NSManaged public var sent: Bool
+    @NSManaged public var recieved: Bool
+    @NSManaged public var time: Date?
+    @NSManaged public var chatLog: Users?
+
+}
+
+extension Chats : Identifiable {
+
+}
